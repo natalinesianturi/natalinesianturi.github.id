e158f79ae176df0f87af736effa980229597ffc0
natalinesianturi <112567938+natalinesianturi@users.noreply.github.com>
2022-09-08 23:32:51 +0700

    Add files via upload

---
+++ luas persegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int s,luas;
+
+	cout<<"Masukkan Sisi Persegi : ";
+	cin>>s;
+
+	luas = s*s;
+	cout<<"luas Persegi adalah "<<luas;
+	return 0;
+}
