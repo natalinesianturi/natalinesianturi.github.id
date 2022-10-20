2e0f1baf139b97f05ed8d97f5d066306e513ae25
natalinesianturi <112567938+natalinesianturi@users.noreply.github.com>
2022-10-20 21:20:42 +0700

    Add files via upload

---
+++ Luas & Keliling Lingkaran.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int jari;
+    cout<<"Masukkan jari-jari lingkaran : ";
+    cin>>jari;
+    double luas=3.14*jari*jari;
+    double keliling=2*3.14*jari;
+    cout<<luas<<endl;
+    cout<<keliling<<endl;
+    
+    return 0;
+}
